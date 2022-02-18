727d3d28d72e57f36c99a89930c93993ae862d9c
fred <domaine75@gmail.com>
2022-02-18 21:44:54 +0100

    Add code

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+  internal class Program
+  {
+    static void Main()
+    {
+      Action<string> Display = Console.WriteLine;
+      Display("Hello World!");
+
+      Display("press any key to exit");
+      Console.ReadKey();
+    }
+  }
+}
